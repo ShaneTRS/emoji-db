5273f0dd0428a1330c62b94b3b5261b9791120d7
Shane <80662335+ShaneTRS@users.noreply.github.com>
2021-06-03 04:35:59 -0700

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,6 +1,6 @@
-sed -i -E 's/alt=":([^:]+):" src="([^"]+)"/\n{"name": "\1","url": "\2"},\n/g' split
-grep '{"name": "' split>tmp
-split -l30 tmp
+sed -i -E 's/alt=":([^:]+):" src="([^"]+)"/\n{"name": "\1","url": "\2"},\n/g' $1
+grep '{"name": "' $1>$1.tmp
+split -l30 $1.tmp
 sed -i '1i{"name": "Emoji","author": "Shane","emotes": [' x*
 sed -i '$ s/.$/]}/' x*
-rm tmp
+rm $1.tmp
